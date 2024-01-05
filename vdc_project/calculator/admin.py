a2d1c83e88454a1826cba30f0303d46d458694e5
Gonzalo Velazquez <gonzalovelazquez@MacBook-Pro-de-Gonzalo.local>
2024-01-05 16:15:16 +0100

    modificacines de vista admin

--- vdc_project/calculator/admin.py
+++ vdc_project/calculator/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+class MedicamentoAdmin(admin.ModelAdmin):
+    filter_horizontal = ("vias_administracion", "especies")
+
 from .models import *
 
 admin.site.register(Especie)
 admin.site.register(ViaAdministracion)
 admin.site.register(UnidadConcentracion)
-admin.site.register(Medicamento)
+admin.site.register(Medicamento, MedicamentoAdmin)
 admin.site.register(Paciente)
