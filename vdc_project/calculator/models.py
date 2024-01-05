74f21f6f2c14fb27b149293cae414a3dd7dbfd22
Gonzalo Velazquez <gonzalovelazquez@MacBook-Pro-de-Gonzalo.local>
2024-01-05 21:38:29 +0100

    database update

--- vdc_project/calculator/models.py
+++ vdc_project/calculator/models.py
@@ -26,15 +26,23 @@ class UnidadConcentracion(models.Model):
         return self.nombre
     
 
+class UnidadDosis(models.Model):
+    nombre = models.CharField(max_length=7, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
 # tabla para medicamentos
 class Medicamento(models.Model):
     nombre = models.CharField(max_length=60)
     dosis_maxima = models.FloatField()
     dosis_minima = models.FloatField()
+    unidad_dosis = models.ForeignKey(UnidadDosis, on_delete=models.CASCADE, null=True)
     concentracion = models.FloatField()
     unidad_concentracion = models.ForeignKey(UnidadConcentracion, on_delete=models.CASCADE, related_name="drogas")
     vias_administracion = models.ManyToManyField(ViaAdministracion)
     especies = models.ManyToManyField(Especie)
+    contraindicaciones=models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return f'{self.nombre}, {self.concentracion} {self.unidad_concentracion}'
