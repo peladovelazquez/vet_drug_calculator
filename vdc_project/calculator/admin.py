803db7e2c57579376497a63cc9a4e2b3100b60ad
Gonzalo Velazquez <gonzalovelazquez@MacBook-Pro-de-Gonzalo.local>
2024-01-05 15:22:05 +0100

    base de datos actualizada

--- vdc_project/calculator/admin.py
+++ vdc_project/calculator/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Especie)
+admin.site.register(ViaAdministracion)
+admin.site.register(UnidadConcentracion)
+admin.site.register(Medicamento)
+admin.site.register(Paciente)
